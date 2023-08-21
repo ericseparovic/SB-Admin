31b53cd65ac308cae8d086f129f86d473d0902f2
Eric Separovic <ericseparovic07@gmail.com>
2023-08-21 08:47:18 -0300

    spring initial

---
+++ src/main/java/com/sbadmin/sbadmin/SbAdminApplication.java
@@ -0,0 +1,13 @@
+package com.sbadmin.sbadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbAdminApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/sbadmin/sbadmin/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.sbadmin.sbadmin;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SbAdminApplication.class);
+	}
+
+}
