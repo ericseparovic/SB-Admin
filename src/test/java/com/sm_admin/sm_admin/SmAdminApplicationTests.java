2cce743ff82bb64338cba25a670a92cc7183177a
Eric Separovic <ericseparovic07@gmail.com>
2023-09-07 09:18:26 -0300

    add sb_admin springboot

---
+++ src/test/java/com/sm_admin/sm_admin/SmAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sm_admin.sm_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
